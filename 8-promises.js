280442ef8cf2846558fa8ec41681d8897699b25a
ko7ko <NLs6houh@protonmail.com>
2022-09-10 15:55:20 +0400

    check out async await

--- 8-promises.js
+++ 8-promises.js
@@ -6,14 +6,27 @@ const doWorkPromise = new Promise((resolve, reject) => {
     }, 2000)
 })
 
-doWorkPromise.then(result => {
-    console.log('Success!', result)
-}).catch(error => {
-    console.log('Error!', error)
-})
+doWorkPromise
+    .then(result => console.log('Success!', result))
+    .catch(error => console.log('Error!', error))
 
 //                         Fulfilled
 //                       /
 //  Promise - Pending -> 
 //                       \
 //                         Rejected
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+add(1, 2).then(sum => {
+    console.log(sum)
+    return add(sum, 5)
+}).then(sum2 => {
+    console.log(sum2)
+}).catch(error => console.log(error))
